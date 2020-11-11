55aff6a2c8686ec89276711b8c92beac7ac1bee1
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-11-11 12:07:03 +0900

    close #2 - fix board list

--- src/Routes/BoardList/presenter.js
+++ src/Routes/BoardList/presenter.js
@@ -3,16 +3,13 @@ import styled from "styled-components";
 const Test = styled.div`
   width: 100px;
   height: 100px;
-  background-color: blue;
+  border: 1px solid blue;
 `;
 
 const BoardList = (props) => {
   const { boardList } = props;
   return (
-    <>
-      <h1>hi I'm board list</h1>
-      {boardList && boardList.map((board) => <Test>{board.title}</Test>)}
-    </>
+    <>{boardList && boardList.map((board) => <Test>{board.title}</Test>)}</>
   );
 };
 
