37129e649a07e81e779941a8fb412836fd0657e9
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-11-18 10:25:15 +0900

    add - board list petch

--- ReduxToolkit/src/App.js
+++ ReduxToolkit/src/App.js
@@ -1,18 +1,26 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchBoardList } from "./slices/boards";
 
 const App = () => {
   const dispatch = useDispatch();
-  const {
-    boards: { boardList },
-  } = useSelector((state) => state);
+  const { boardList } = useSelector((state) => state.boards);
 
   useEffect(() => {
     dispatch(fetchBoardList());
+    console.log("effect");
+    return () => {
+      console.log("cleanup");
+    };
   }, [dispatch]);
 
-  return boardList.map((item) => <div>{item.title}</div>);
+  return (
+    <>
+      {boardList.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </>
+  );
 };
 
 export default App;
